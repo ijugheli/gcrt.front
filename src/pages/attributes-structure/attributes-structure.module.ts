6505347a9cf9944cd2a02fb52254dca37a3d33af
ijugheli <ijugh13@freeuni.edu.ge>
2023-01-13 18:08:49 +0400

    Changes

--- src/pages/attributes-structure/attributes-structure.module.ts
+++ src/pages/attributes-structure/attributes-structure.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AttributesStructureComponent } from './attributes-structure.component';
+import { TableModule } from 'primeng/table';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    BrowserAnimationsModule,
+    TableModule,
+    FormsModule,
   ],
   declarations: [AttributesStructureComponent]
 })
